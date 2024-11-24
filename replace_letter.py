21841831a1f6911c6be8ef88b588cede726cfc1c
Anta101 <m5901112@gmail.com>
2024-11-24 12:50:15 +0300

    Update replace_letter.py

--- replace_letter.py
+++ replace_letter.py
@@ -5,14 +5,12 @@ from dotenv import load_dotenv
 load_dotenv('method.env')
 DATABASE_LOGIN  = os.getenv('DATABASE_LOGIN')
 YA_PASSWORD = os.getenv('YA_PASSWORD')
-FROM = os.getenv('FROM')
-TO = os.getenv('TO')
+
 letter = """\
-FROM
-TO
+From: {from_email}
+To: {to_email}
 Subject: Приглашение
 Content-Type: text/plain; charset="UTF-8;
-
 Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
 {website} — это новая версия онлайн-курса по программированию. 
 Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
@@ -27,10 +25,10 @@ Content-Type: text/plain; charset="UTF-8;
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 Регистрируйся → {website} 
 На курсы, которые еще не вышли, можно подписаться и получить уведомление
-о релизе сразу на имейл.""".format(friend_name ='Иван', my_name ='Devman', website ='https://dvmn.org/profession-ref-program/')
+о релизе сразу на имейл.""".format(from_email = 'devmanorg@yandex.ru', to_email= 'anta01@yandex.ru', friend_name ='Ivan', my_name ='Devman', website ='https://dvmn.org/profession-ref-program/')
 letter = letter.encode('UTF-8')
 server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
 server.login(DATABASE_LOGIN, YA_PASSWORD)
-server.sendmail(DATABASE_LOGIN, TO, letter)
+server.sendmail(DATABASE_LOGIN, 'anta01@yandex.ru', letter)
 
 server.quit()
